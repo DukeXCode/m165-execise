785ad221d24de137c394b0e7fd2c871cb9e3f585
DukeXCode <naim.giger@outlook.com>
2023-06-09 11:03:43 +0200

    implement all remaining functionality

--- db_interactions.py
+++ db_interactions.py
@@ -7,3 +7,9 @@ class DbInteractions:
 
     def insert_software(self, fields: dict):
         self.client['softwares'].insert_one(fields)
+
+    def insert_key(self, software_id, key):
+        return self.client['keys'].insert_one({'software_id': software_id, 'key': key})
+
+    def get_key(self, software_id):
+        return self.client['keys'].find({'software_id': software_id})['key']
--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import os
 
 import pymongo
 import db_interactions
-import software
 
 
 def create_database_if_needed(name: str):
@@ -25,15 +24,32 @@ db_interactions = db_interactions.DbInteractions(client)
 softwares_db = create_database_if_needed('softwares')
 keys_db = create_database_if_needed('keys')
 
-if softwares_db is None or keys_db is None:
-    print('Required DB is not available')
-else:
+
+def run_interface():
+    global software
     action = input('Select action (i, k, ik, q): ')
     if action == 'q':
         print('Goodbye!')
     elif action == 'i':
-        software_input = input('Enter software name and manufacturer')
+        software_input = input('Enter software name and manufacturer: ')
         name = software_input.split(' ')[0]
         manufacturer = software_input.split(' ')[1]
         software = software.Software(name, manufacturer)
         db_interactions.insert_software(software.__dict__)
+        run_interface()
+    elif action == 'ik':
+        key_input = input('Enter software id and key: ')
+        software_id = key_input.split(' ')[0]
+        key = key_input.split(' ')[1]
+        db_interactions.insert_key(software_id, key)
+        run_interface()
+    elif action == 'k':
+        input_software_id = input('Enter the software id: ')
+        print(db_interactions.get_key(input_software_id))
+        run_interface()
+
+
+if softwares_db is None or keys_db is None:
+    print('Required DB is not available')
+else:
+    run_interface()
